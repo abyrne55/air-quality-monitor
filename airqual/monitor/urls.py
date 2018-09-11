af682b8cca3abf090fe87ff41ab8b463df73d1d4
Anthony Byrne <anthony@abyrne.me>
2018-09-11 17:37:02 -0400

    Added some skeleton views

---
+++ airqual/monitor/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'monitor'
+urlpatterns = [
+    # ex: /monitor/
+    path('', views.index, name='index'),
+    # ex: /monitor/login/
+    path('login/', views.login, name='login'),
+]
--- airqual/monitor/views.py
+++ airqual/monitor/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # Renders the home page
+    return render(request, 'monitor/index.html')
+
+def login(request):
+    # Renders the login screen
+    return render(request, 'monitor/login.html')
