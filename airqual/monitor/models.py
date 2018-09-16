7791d0de36d84e47736a9bc03ce4b28ff1bd5270
Huzefa Mandviwala <hzmandviwala@gmail.com>
2018-09-15 21:23:01 -0400

    Sample model for datapoint and creation via POST

--- airqual/monitor/models.py
+++ airqual/monitor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 # Create your models here.
+
+# Data point model - to be created by POST requests by sensor
+class DataPoint(models.Model):
+    sensor = models.CharField(max_length=200)
+    value = models.IntegerField(default=0)
+    #timestamp = models.DateTimeField('timestamp')
+    def __str__(self):
+        return self.value
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    curr_value = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+
--- airqual/monitor/views.py
+++ airqual/monitor/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.http import HttpResponse, Http404
+from .models import Question, DataPoint
+
+
+def index(request):
+    if request.method == 'POST':
+        return HttpResponse('POST request')
+
+    else:
+        return HttpResponse('GET request')
+
+# TODO - get info from request into datapoint
+def addDataPoint
+    if request.method == 'POST':
+        datapoint = DataPoint.objects.create(sensor="test", value=5)
+        return HttpResponse('POST to add datapoint')
