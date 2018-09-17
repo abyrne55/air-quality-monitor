db4c3c4d4bdef3d17d1766cfffbbd7c8d210ab37
Huzefa Mandviwala <hzmandviwala@gmail.com>
2018-09-17 09:27:34 -0400

    Framework for adding data points

--- airqual/monitor/models.py
+++ airqual/monitor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DataPoint(models.Model):
+    value = models.IntegerField(default = 0)
+
+class Sensor(models.Model):
+    #owner
+    #values
+    #name
+
--- airqual/monitor/views.py
+++ airqual/monitor/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("index")
+
+def addDataPoint(request):
+    return HttpResponse("add data point")
