90bbd85c870957927d3f88cc3ce4ea4fcacd082d
Huzefa Mandviwala <hzmandviwala@gmail.com>
2018-09-17 12:51:02 -0400

    Basic API key system

--- airqual/monitor/models.py
+++ airqual/monitor/models.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 class DataPoint(models.Model):
     value = models.IntegerField(default = 0)
     timestamp = models.DateTimeField('date added')
-    owner = models.CharField(max_length=200, default='no owner')
+    sensor = models.CharField(max_length=200, default='no associated sensor')
 
 class Sensor(models.Model):
-    #owner
-    values = []
+    owner = models.CharField(max_length=200, default='no owner')
     sensor_id = models.CharField(max_length=200, default = 'default')
+    
     def __str__(self):
         return self.sensor_id
--- airqual/monitor/views.py
+++ airqual/monitor/views.py
@@ -2,25 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import DataPoint, Sensor
-from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
     return HttpResponse("index")
 
-@csrf_exempt
 def addDataPoint(request):
-    #todo here - API key
+    api_key = request.GET.get('api_key')
+    if (api_key != 'complicatedstring'):
+        return HttpResponse("Invalid API key")
    
     sensor_id_arg = request.GET.get('sensor_id')
     sensor_object = get_object_or_404(Sensor, sensor_id=sensor_id_arg)
     
-    #at this point, we know we have a valid sensor
+    #at this point, we know we have a valid sensor and sensor_id
     
     #construct the datapoint
     val = request.GET.get('value')
-    datapoint = DataPoint(value = val, timestamp = timezone.now(), owner=sensor_id_arg)
+    datapoint = DataPoint(value = val, timestamp = timezone.now(), sensor=sensor_id_arg)
     datapoint.save()
-    #assign the datapoint to the sensor it belongs to
-    sensor_object.values.append(datapoint)
+    
     return HttpResponse("added data point")
